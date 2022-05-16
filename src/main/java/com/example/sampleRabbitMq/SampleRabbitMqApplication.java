b8a59c3a3d9291a807b6368d9b746870c1d8bd38
Lahiru <lahirus@treinetic.com>
2022-05-16 15:33:50 +0530

    first commit

---
+++ src/main/java/com/example/sampleRabbitMq/SampleRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleRabbitMq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleRabbitMq/SampleRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleRabbitMq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
